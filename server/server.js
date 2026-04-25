2d1c0463b28eff82e16618066f55cb9a11f778d4
LALo99-vk <jharshav442@gmail.com>
2026-04-25 17:41:10 +0530

    third  commit and sending to client for testing

--- server/server.js
+++ server/server.js
@@ -60,8 +60,10 @@ app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 export default app;
